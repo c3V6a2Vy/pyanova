d99ded4ac2f5e6e7cbe2214636a3446d960a6cf1
c3V6a2Vy <c3V6a2Vy@protonmail.com>
2019-09-06 00:53:43 -0700

    added python3 support for the example as well

--- samples/pyanova_terminal.py
+++ samples/pyanova_terminal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- mode: python; coding: utf-8 -*-
 
 # Copyright (C) 2017, c3V6a2Vy <c3V6a2Vy@protonmail.com>
@@ -10,21 +10,22 @@
 """
 import re
 import pyanova
+from __future__ import print_function
 
 if __name__ == '__main__':
-    print '~~ pyanova demo ~~'
-    print '-- Initializing PyAnova object'
+    print('~~ pyanova demo ~~')
+    print('-- Initializing PyAnova object')
     pa = pyanova.PyAnova(debug=True)
     cmd_re = re.compile('^get|^stop|^set')
     cmd_list = list(filter(lambda m: cmd_re.match(m), dir(pa)))
-    print '-- PyAnova object initialized'
-    print '-- Available commands:\n    %s'%'\n    '.join(cmd_list)
-    print '-- Type commands like: \'get_current_temperature()\' or \'set_temperature(42)\''
-    print '-- Type \'bye\' to end demo'
+    print('-- PyAnova object initialized')
+    print('-- Available commands:\n    %s'%'\n    '.join(cmd_list))
+    print('-- Type commands like: \'get_current_temperature()\' or \'set_temperature(42)\'')
+    print('-- Type \'bye\' to end demo')
     while True:
         ri = raw_input('> ')
         if ri.lower().startswith('bye'):
-            print 'cya.'
+            print('cya.')
             break
         else:
-            print '< ' + eval('pa.'+ri)
+            print('< ' + eval('pa.'+ri))
